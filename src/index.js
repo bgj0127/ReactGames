bead92cda98af3f713164f019e91eb33660294dd
Bae gil jun <51225580+bgj0127@users.noreply.github.com>
2020-06-30 10:48:05 +0900

    Baseball Game 클래스형 컴포넌트로 숫자야구 게임을 만들며 map() 함수를 배웠다.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import WordRelay from "./WordRelay";
+import Baseball from "./Baseball";
 // import App from "./App";
 
-ReactDOM.render(<WordRelay />, document.getElementById("root"));
+ReactDOM.render(<Baseball />, document.getElementById("root"));
