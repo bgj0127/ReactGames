f6d93c03cbd9dac6b60f829803ade9bd1876264a
Bae gil jun <51225580+bgj0127@users.noreply.github.com>
2020-07-01 23:31:15 +0900

    update

--- src/ResponseCheck.js
+++ src/ResponseCheck.js
@@ -4,29 +4,30 @@ const ResponseCheck = () => {
   const [message, setMessage] = useState("클릭해서 시작하세요.");
   const [state, setState] = useState("waiting");
   const [result, setResult] = useState([]);
-  const timeOut = useRef(null);
+  const timeout = useRef(null);
   const startTime = useRef();
   const endTime = useRef();
-
 
   const clickScreen = () => {
     if (state === "waiting") {
       setMessage("초록색이 되면 클릭하세요");
       setState("ready");
-      setTimeout(() => {
+      timeout.current = setTimeout(() => {
         setMessage("지금 클릭");
+        startTime.current = new Date();
         setState("now");
       }, Math.floor(Math.random() * 1000) + 7000); // 1 ~ 8초 랜덤
     } else if (state === "ready") {
       // 이른 클릭
-      setMessage("ㅋㅋ! 아직 빨간색인데~");
-      clearTimeout(timeout);
+      setMessage("ㅋㅋ! 아직 아닌데~");
+      clearTimeout(timeout.current);
       setState("waiting");
     } else if (state === "now") {
       // 반응속도 체크
+      endTime.current = new Date();
       setState("waiting");
-      setResult((prev)=>{
-        return [...prev, ]
+      setResult(prev => {
+        return [...prev, endTime.current - startTime.current];
       });
       setMessage("클릭해서 시작하세요");
     }
@@ -42,8 +43,10 @@ const ResponseCheck = () => {
         {message}
       </div>
       {result.length === 0 ? null : (
-        <div>time: {result.reduce((a, c) => a + c) / result.length}ms</div>
-        <button onClick={onReset}>Reset</button>
+        <>
+          <div>time: {result.reduce((a, c) => a + c) / result.length}ms</div>
+          <button onClick={onReset}>Reset</button>
+        </>
       )}
     </>
   );
