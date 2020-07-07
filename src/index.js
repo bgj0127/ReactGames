a9de65447e4ba772e5e1f273381c49534076c020
Bae gil jun <51225580+bgj0127@users.noreply.github.com>
2020-07-07 23:23:36 +0900

    react

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import WordRelay from "./WordRelay";
 import ResponseCheck from "./ResponseCheck";
-import RSP from "./RSP";
 // import App from "./App";
 
-ReactDOM.render(<RSP />, document.getElementById("root"));
+ReactDOM.render(<ResponseCheck />, document.getElementById("root"));
